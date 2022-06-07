2151bcbc367110ea7d8d85ab4aac8e9c34685e8e
( Odi ) Thanapat Sombatkamrai <30354428+MSMasterX@users.noreply.github.com>
2022-06-07 13:02:45 +0200

    Average number

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 23;
+        int number2 = 11;
+        int number3 = 77;
+        System.out.println((number1+number2+number3)/3);
+    }
+}
